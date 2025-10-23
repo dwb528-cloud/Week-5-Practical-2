1ffc9962344e2aa797caa02c9d2875cf9b5ff5dd
dwb528-cloud <dwb528@york.ac.uk>
2025-10-23 12:29:03 +0100

    Finished Week 5 Practical 2

---
+++ exercise1-1/sketch.js
@@ -0,0 +1,36 @@
+let rects = [];
+
+function setup() {
+    createCanvas(600, 600);
+    createRects();
+}
+
+function draw() {
+    background (253, 218, 13);
+    drawRects();
+    for (let i = 0; i < rects.length; i++) {
+        if (i % 2) {
+        fill (0);
+    } else {
+        fill (255);
+    }
+    }
+}
+
+function createRects() {
+    for (let i = 0; i < 100; i++) {
+        let randomrect = {
+            x: random(0, 600),
+            y: random(0, 600),
+            w: random(5, 10),
+            h: random(5, 10)
+        }
+        rects.push(randomrect);
+    }
+}
+
+function drawRects() {
+    for(let i in rects) {
+        rect(rects[i].x, rects[i].y, rects[i].w, rects[i].h);
+    }
+}
